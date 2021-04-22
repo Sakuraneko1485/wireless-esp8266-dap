3837c364d647ffc50d25eddd398b98aaafa6763d
Sakuraneko1485 <56515812+Sakuraneko1485@users.noreply.github.com>
2021-04-23 02:34:14 +0800

    Update wifi_configuration.h

--- main/wifi_configuration.h
+++ main/wifi_configuration.h
@@ -10,8 +10,8 @@
 #ifndef __WIFI_CONFIGURATION__
 #define __WIFI_CONFIGURATION__
 
-#define WIFI_SSID "DAP"
-#define WIFI_PASS "12345678"
+#define WIFI_SSID "CatHome"
+#define WIFI_PASS "CatHome520"
 
 #define PORT 3240
 
